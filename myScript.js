ef95836d9474708dd849a220e466e8e68bb50089
Boston22 <csacadamy@gmail.com>
2022-01-31 09:41:32 -0800

    added base format to js file

--- myScript.js
+++ myScript.js
@@ -1,14 +1,16 @@
 
 
 
+$(document).ready(function)() {
+  $("#imagetoggler").click(function(){
+  $("img").toggle();
+  });
+  $("#p1").click(function(){
+  	$("#p2").toggle();
+  	$("#p3").toggle();
+  	});
 
-
-
-
-
-
-
-
+});
 
 
 
